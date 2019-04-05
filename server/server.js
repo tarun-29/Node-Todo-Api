ca4b412b6b9b02fecfbdd4a94c2e35f80e779b36
Tarun Kantiwal <29kantiwaltarun@gmail.com>
2019-04-05 22:43:47 +0530

    Add Delete Route

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
+// const port = process.env.PORT || 3000
 
 var app = express();
 
@@ -49,8 +50,24 @@ app.get('/todos/:id',(req,res)=>{
     //404
 })
 
-app.listen('8000',()=>{
-    console.log("Todo Server Started");
+app.delete('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send()
+    }
+    Todo.findByIdAndRemove(id).then((todo)=>{
+        if(!todo){
+            return res.status(404).send()
+        }
+        res.send(todo)
+    }).catch((e)=>{
+        res.status(400).send()
+    });
+})
+
+app.listen(8000,()=>{
+    console.log(`Todo Server Started at`);
 })
 
 module.exports = {app}
